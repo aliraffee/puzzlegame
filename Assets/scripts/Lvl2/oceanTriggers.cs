e808bce2e11b58e7393e479a1102f7526d3213c0
GabyHon <gabriella.hon888+coding@gmail.com>
2023-08-15 12:08:41 +0200

    Added sprites for sea and rubbish
    Added scripts and sprites to scene

---
+++ Assets/scripts/Lvl2/oceanTriggers.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oceanTriggers : MonoBehaviour
+{
+    [SerializeField] private GameObject oceanDrag;
+    [SerializeField] private GameObject[] triggers;
+
+    [Header("Trigger bools")]
+    public bool triggered1 = false;
+    public bool triggered2 = false;
+    public bool triggered3 = false;
+    public bool triggered4 = false;
+    public bool triggered5 = false;
+    public bool triggered6 = false;
+
+
+}
