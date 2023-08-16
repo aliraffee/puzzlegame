663840fa5fadc326f66edeece701c825039015f4
GabyHon <gabriella.hon888+coding@gmail.com>
2023-08-16 22:43:18 +0200

    Ocean puzzle
    Added blue strip for boarders.
    Hint button becomes not interactable once clicked.
    If the trigger count reaches the number of triggers, the letter c would appear.
    TriggerPlate tag added.
    Hint text added for hint 1.
    Hint 2 button added which will show the arrows.

--- Assets/scripts/Lvl2/oceanTriggers.cs
+++ Assets/scripts/Lvl2/oceanTriggers.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class oceanTriggers : MonoBehaviour
 {
     [SerializeField] private GameObject oceanDrag;
-    [SerializeField] private GameObject[] triggers;
     [SerializeField] private GameObject LetterC;
+    [SerializeField] private GameObject[] triggers;
     [SerializeField] private GameObject[] arrows;
 
-    [Header("Trigger bools")]
-    public bool triggered1 = false;
-    public bool triggered2 = false;
-    public bool triggered3 = false;
-    public bool triggered4 = false;
-    public bool triggered5 = false;
-    public bool triggered6 = false;
+    [SerializeField] Button HintBtn1;
+    [SerializeField] Button HintBtn2;
+    [SerializeField] TMP_Text HintTxt;
+
+    private Vector3 offSet;
+    private Camera cam;
+
+    [SerializeField] private int triggerCount;
+
+    private void Start()
+    {
+        HintTxt.text = " ";
+        HintBtn2.interactable = false;
+    }
+
+    public void HintforSeaTxt()
+    {
+        HintTxt.text = "I wonder where the ocean went... ";
+
+        HintBtn1.interactable = false;
+        HintBtn2.interactable = true;
+    }
 
     public void HintforSea()
     {
@@ -25,11 +42,44 @@ public class oceanTriggers : MonoBehaviour
         arrows[3].SetActive(true);
         arrows[4].SetActive(true);
         arrows[5].SetActive(true);
+
+        HintBtn2.interactable = false;
+    }
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnMouseDown()
+    {
+        offSet = transform.position - GetMousePos();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMousePos() + offSet;
+    }
+
+    Vector3 GetMousePos()
+    {
+        var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("TriggerPlate"))
+        {
+            triggerCount++;
+            Debug.Log("Triggerd!");
+        }
     }
 
     private void Update()
     {
-        if (triggered1 == true && triggered2 == true && triggered3 == true && triggered4 == true && triggered5 == true && triggered6 == true)
+        if (triggerCount == 6)
         {
             LetterC.SetActive(true);
         }
