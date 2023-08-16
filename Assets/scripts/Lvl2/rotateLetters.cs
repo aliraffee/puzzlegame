6b7fed7293c22dcb3d3807c4174579cb7b0c43b9
GabyHon <gabriella.hon888+coding@gmail.com>
2023-08-16 13:11:48 +0200

    Separate buttons for each piece
    Set separate buttons to rotate the different buttons.
    Checking how many times the button is clicked then disabling the buttons when they have been clicked the number of times to rotate the lightning bolt piece back to the correct rotation.
    
    Deleted full lightning bolt image.
    
    If the button are disabled, the rokLetters gameobject will become active.

--- Assets/scripts/Lvl2/rotateLetters.cs
+++ Assets/scripts/Lvl2/rotateLetters.cs
@@ -10,18 +10,85 @@ public class rotateLetters : MonoBehaviour
     [SerializeField] private GameObject bottomR;
     [SerializeField] private GameObject bottomL;
 
-    public void TopRightAntiClockwise()
-    {
-        topR.transform.Rotate(0f, 0f, 20f);
-    }
+    [Header("Btn Click number")]
+    [SerializeField] int TopRButtonClick = 0;
+    [SerializeField] int TopLButtonClick = 0;
+    [SerializeField] int BottomRButtonClick = 0;
+    [SerializeField] int BottomLButtonClick = 0;
+
+    [Header("Btns")]
+    [SerializeField] Button TopRBtn;
+    [SerializeField] Button TopLBtn;
+    [SerializeField] Button BottomRBtn;
+    [SerializeField] Button BottomLBtn;
+
+    [SerializeField] GameObject rokLetters;
 
     public void TopRightClockwise()
     {
         topR.transform.Rotate(0f, 0f, -20f);
+        TopRButtonClick++;
+        TopRBtnDisabled();
+    }
+
+    public void TopLeftClockwise()
+    {
+        topL.transform.Rotate(0f, 0f, -20f);
+        TopLButtonClick++;
+        TopLBtnDisabled();
+    }
+
+    public void BottomRightClockwise()
+    {
+        bottomR.transform.Rotate(0f, 0f, -20f);
+        BottomRButtonClick++;
+        BottomRBtnDisabled();
+    }
+
+    public void BottomLeftClockwise()
+    {
+        bottomL.transform.Rotate(0f, 0f, -20f);
+        BottomLButtonClick++;
+        BottomLBtnDisabled();
+    }
+
+    public void TopRBtnDisabled()
+    {
+        if (TopRButtonClick == 4)
+        {
+            TopRBtn.interactable = false;
+        }
+    }
+
+    public void TopLBtnDisabled()
+    {
+        if (TopLButtonClick == 17)
+        {
+            TopLBtn.interactable = false;
+        }
+    }
+
+    public void BottomRBtnDisabled()
+    {
+        if (BottomRButtonClick == 14)
+        {
+            BottomRBtn.interactable = false;
+        }
+    }
+
+    public void BottomLBtnDisabled()
+    {
+        if (BottomLButtonClick == 16)
+        {
+            BottomLBtn.interactable = false;
+        }
     }
 
     private void Update()
     {
-        
+        if(TopRButtonClick == 4 && TopLButtonClick == 17 && BottomRButtonClick == 14 && BottomLButtonClick == 16)
+        {
+            rokLetters.SetActive(true);
+        }
     }
 }
