8c174323d77cf9d8341b713bc259f4deea468d57
GabyHon <gabriella.hon888+coding@gmail.com>
2023-08-16 13:50:45 +0200

    Hint for ocean
    Deleted instantiate script.
    Button for the hint, will show the arrows.

--- Assets/scripts/Lvl2/oceanTriggers.cs
+++ Assets/scripts/Lvl2/oceanTriggers.cs
@@ -6,6 +6,8 @@ public class oceanTriggers : MonoBehaviour
 {
     [SerializeField] private GameObject oceanDrag;
     [SerializeField] private GameObject[] triggers;
+    [SerializeField] private GameObject LetterC;
+    [SerializeField] private GameObject[] arrows;
 
     [Header("Trigger bools")]
     public bool triggered1 = false;
@@ -15,5 +17,22 @@ public class oceanTriggers : MonoBehaviour
     public bool triggered5 = false;
     public bool triggered6 = false;
 
+    public void HintforSea()
+    {
+        arrows[0].SetActive(true);
+        arrows[1].SetActive(true);
+        arrows[2].SetActive(true);
+        arrows[3].SetActive(true);
+        arrows[4].SetActive(true);
+        arrows[5].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (triggered1 == true && triggered2 == true && triggered3 == true && triggered4 == true && triggered5 == true && triggered6 == true)
+        {
+            LetterC.SetActive(true);
+        }
+    }
 
 }
