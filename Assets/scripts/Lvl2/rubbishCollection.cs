26fa5cecc4c2247ee8275730f38e9b6a7a599a01
GabyHon <gabriella.hon888+coding@gmail.com>
2023-08-16 22:21:33 +0200

    Rubbish collection
    Dragging rubbish into bin, destroys rubbish.
    Counter to check if all the rubbish has collided and destroyed, if so the letters "in" gameobject would be active.

--- Assets/scripts/Lvl2/rubbishCollection.cs
+++ Assets/scripts/Lvl2/rubbishCollection.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class rubbishCollection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 offSet;
+    private Camera cam;
+
+    [SerializeField] private int rubbishCount;
+    [SerializeField] private GameObject inLetters;
+
+    private void Awake()
     {
-        
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        offSet = transform.position - GetMousePos();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMousePos() + offSet;
+    }
+
+    Vector3 GetMousePos()
+    {
+        var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Rubbish"))
+        {
+            Destroy(collision.gameObject);
+            rubbishCount++;
+            Debug.Log("Rubbish destroyed!");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Boarder")
+        {
+            Debug.Log("This is a boarder");
+        }
+    }
+
+    private void Update()
+    {
+        if (rubbishCount == 5)
+        {
+            inLetters.SetActive(true);
+        }
     }
 }
