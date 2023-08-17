01eba98301d407d9ea2627ec47a821f58f299f7d
aliraffee <2586807@students.wits.ac.za>
2023-08-17 12:01:15 +0200

    add oncollisison for letter
    ivys push had issues with this so just added it

--- Assets/scripts/draggingletters.cs
+++ Assets/scripts/draggingletters.cs
@@ -9,6 +9,7 @@ public class draggingletters : MonoBehaviour
     Vector2 buffer = Vector2.zero;
     Vector2 tomve = Vector2.zero;
     private float speed=0.02f;
+    public GameObject ball;
 
     public void FixedUpdate()
     {
@@ -23,6 +24,20 @@ public class draggingletters : MonoBehaviour
     {
         tomve = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - buffer;
         transform.position = Vector2.MoveTowards(transform.position, tomve, speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+        if (collision.gameObject.name == "ll" && this.gameObject.name == "ba")
+        {
+            this.gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
+            ball.SetActive(true);
+
+
+        }
     }
 }
 
