39b6eb63cc757e711fa9806dee8f6e648d406fb0
aliraffee <2586807@students.wits.ac.za>
2023-08-17 11:45:45 +0200

    change how collisison works

--- Assets/scripts/draggingletters.cs
+++ Assets/scripts/draggingletters.cs
@@ -9,6 +9,9 @@ public class draggingletters : MonoBehaviour
     Vector2 buffer = Vector2.zero;
     Vector2 tomve = Vector2.zero;
     private float speed=0.02f;
+    public GameObject ball;
+    int i=0;
+    letterobject lb,otherlb;
 
     public void FixedUpdate()
     {
@@ -23,6 +26,27 @@ public class draggingletters : MonoBehaviour
     {
         tomve = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - buffer;
         transform.position = Vector2.MoveTowards(transform.position, tomve, speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.position.y>transform.position.y)
+        {
+            for (int i = 0; i < otherlb.let.Capacity; i++)
+            {
+                lb.let.Add(otherlb.let[i]);
+            }
+        }
+
+
+        if (collision.gameObject.name=="ll"&& this.gameObject.name=="ba")
+                {
+            this.gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
+            ball.SetActive(true);
+            
+
+                }
     }
 }
 
