308b59208e6d1772732c2ef09d2ac44092cc90bc
aliraffee <2586807@students.wits.ac.za>
2023-08-19 12:21:01 +0200

    finished

--- Assets/scripts/ttoriallvl2/movingroad.cs
+++ Assets/scripts/ttoriallvl2/movingroad.cs
@@ -7,11 +7,11 @@ public class movingroad : MonoBehaviour
     // Start is called before the first frame update
     private void FixedUpdate()
     {
-        if (this.transform.position.y<-2.6)
+        if (this.transform.position.y<-5)
         {
-            this.transform.position = new Vector3(transform.position.x,2.6f, transform.position.z);
+            this.transform.position = new Vector3(transform.position.x,5.53f, transform.position.z);
         }
-        this.transform.position = new Vector3(transform.position.x, transform.position.y-0.0f, transform.position.z);
+        this.transform.position = new Vector3(transform.position.x, transform.position.y-0.01f, transform.position.z);
 
     }
 }
